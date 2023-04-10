23b0ce6da269bdc4b8a35b47de251b5ad2de7124
reloginn <nikita.malina23@gmail.com>
2023-04-10 17:42:38 +0600

    1

--- amd_webui.py
+++ amd_webui.py
@@ -10,26 +10,15 @@ import numpy as np
 import random
 import datetime
 from PIL import Image
-#from modules import txt2img
 
 
 python = sys.executable
-#repositories = pathlib.Path().absolute() / 'repositories'
 
 onnx_dir = pathlib.Path().absolute()/'onnx_models'
 output_dir = pathlib.Path().absolute()/'output'
 
 if not output_dir.exists():
         output_dir.mkdir(parents=True, exist_ok=True)
-#from PIL import Image
-#baseImage = Image.open(r"in.jpg").convert("RGB") # opens an image directly from the script's location and converts to RGB color profile
-#baseImage = baseImage.resize((768,512))
-
-prompt = "A fantasy landscape, trending on artstation"
-#denoiseStrength = 0.8 # a float number from 0 to 1 - decreasing this number will increase result similarity with baseImage
-#scale = 7.5
-#pipe = None
-##need to set up UI for downloading weights
 
 
 def txt2img(prompt, negative_prompt, steps, height, width, scale, denoise_strength=0, seed=None, scheduler=None, num_image=None):
@@ -38,25 +27,21 @@ def txt2img(prompt, negative_prompt, steps, height, width, scale, denoise_streng
         if seed < 0:
             seed = random.randint(0,4294967295)
     except:
-        seed = random.randint(0, 4294967295) # 2^32
+        seed = random.randint(0, 4294967295)
         
     generator = np.random.RandomState(seed)
         
-    #generator = torch.Generator(device='cpu')
-    #generator = generator.manual_seed(seed)
     image = pipe(prompt,
                 negative_prompt = negative_prompt,
                 num_inference_steps=steps,
                 height = height,
                 width = width,
                 guidance_scale=scale,
-                #strength=denoise_strength,
                 generator = generator,
                 num_images_per_prompt = num_image
                 
                 ).images[0]
-    
-    ## autosave img
+
     img_name = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M') + ".png"
     image.save(output_dir/img_name)
     return image
@@ -70,7 +55,7 @@ def img2img(prompt, negative_prompt, image_input, steps, height, width, scale, d
         seed = int(seed)
         if seed < 0:
             seed = random.randint(0,4294967295)
-    print(f'this is the seed {seed}')
+    print(f'Ключ генерации: {seed}')
         
     generator = np.random.RandomState(seed)
     print(image_input)
@@ -82,11 +67,8 @@ def img2img(prompt, negative_prompt, image_input, steps, height, width, scale, d
                 negative_prompt = negative_prompt,
                 num_images_per_prompt = num_image,
                 generator = generator,
-                #height = height,
-                #width = width
                 ).images[0]
-    
-    ## autosave img
+                
     img_name = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M') + ".png"
     image.save(output_dir/img_name)
     
@@ -95,16 +77,15 @@ def img2img(prompt, negative_prompt, image_input, steps, height, width, scale, d
 
 def huggingface_login(token):
     try:
-        #output = _login._login(HfApi(), token = token)
         output = _login._login(token = token, add_to_git_credential = True)
-        return "Login successful."
+        return "Успешно."
     except Exception as e:
         return str(e)
     
 
 
 def pip_install(lib):
-    subprocess.run(f'echo Installing {lib}...', shell=True)
+    subprocess.run(f'echo Устанавливаю {lib}...', shell=True)
     if 'ort_nightly_directml' in lib:
         subprocess.run(f'echo 1', shell=True)
         subprocess.run(f'echo "{python}" -m pip install {lib}', shell=True)
@@ -115,7 +96,7 @@ def pip_install(lib):
         subprocess.run(f'"{python}" -m pip install {lib}', shell=True, capture_output=True)
 
 def pip_uninstall(lib):
-    subprocess.run(f'echo Uninstalling {lib}...', shell=True)
+    subprocess.run(f'echo Удаляю {lib}...', shell=True)
     subprocess.run(f'"{python}" -m pip uninstall -y {lib}', shell=True, capture_output=True)
 
 def is_installed(lib):
@@ -138,9 +119,6 @@ def download_sd_model(model_path):
     convert_stable_diffusion_checkpoint_to_onnx.convert_models(model_path, str(onnx_model_dir), onnx_opset, onnx_fp16)
     pip_uninstall('onnx')
 
-    
-#'CompVis/stable-diffusion-v1-4'
-
 
 def display_onnx_models():
     if not onnx_dir.exists():
@@ -149,110 +127,94 @@ def display_onnx_models():
 
 
 def load_onnx_model(model):
-##    if is_installed('onnx'):
-##        pip_uninstall('onnx')
-##    onnx_nightly = pathlib.Path().absolute()/'repositories/ort_nightly_directml-1.13.0.dev20220908001-cp39-cp39-win_amd64.whl'
-##    pip_install(str(onnx_nightly))
-##    subprocess.run('echo installing onnx nightly built', shell=True)
     global pipe
     pipe = OnnxStableDiffusionPipeline.from_pretrained(str(onnx_dir/model),
                                                        safety_checker = None,
                                                        provider="DmlExecutionProvider")
     
-    return 'model ready'
+    return 'Модель готова'
 
 
 def load_onnx_model_i2i(model):
-##    if is_installed('onnx'):
-##        pip_uninstall('onnx')
-##    onnx_nightly = pathlib.Path().absolute()/'repositories/ort_nightly_directml-1.13.0.dev20220908001-cp39-cp39-win_amd64.whl'
-##    pip_install(str(onnx_nightly))
-##    subprocess.run('echo installing onnx nightly built', shell=True)
     global pipe_img2img
     pipe_img2img = OnnxStableDiffusionImg2ImgPipeline.from_pretrained(str(onnx_dir/model),
                                                               safety_checker = None,
                                                               provider="DmlExecutionProvider")
-    return 'model ready'
+    return 'Модель готова'
 
 
 def start_app():
     with gr.Blocks() as app:
-        gr.Markdown('STABLE DIFFUSION WEBUI FOR AMD')
+        gr.Markdown('StableDiffusion WebUI для видеокарт AMD на операционной системе Windows')
         with gr.Tab('txt2img'):
-            txt2img_prompt_input = gr.Textbox(label='Prompt')
-            txt2img_negative_prompt_input = gr.Textbox(label='Negative Prompt')
+            txt2img_prompt_input = gr.Textbox(label='Запрос')
+            txt2img_negative_prompt_input = gr.Textbox(label='Отрицательный запрос')
             with gr.Row():
                 with gr.Column(scale = 1):
 
                     with gr.Row():
-                        txt2img_model_input = gr.Dropdown(label='Select a model', choices = display_onnx_models())
-                        test_output = gr.Textbox(label='testing output')
-                    inference_step_input = gr.Slider(label='Steps', value = 30, minimum = 0, maximum=200, step = 1)
+                        txt2img_model_input = gr.Dropdown(label='Выберите модель:', choices = display_onnx_models())
+                        test_output = gr.Textbox(label='Статус модели')
+                    inference_step_input = gr.Slider(label='Шаги', value = 30, minimum = 5, maximum=100, step = 1)
                     with gr.Row():
-                        image_height = gr.Slider(label='Height', value = 512, minimum = 0, maximum=1080, step = 64)
-                        image_width = gr.Slider(label='Width', value = 512, minimum = 0, maximum=1080, step = 64)
+                        image_height = gr.Slider(label='Высота', value = 512, minimum = 0, maximum=2048, step = 32)
+                        image_width = gr.Slider(label='Ширина', value = 512, minimum = 0, maximum=2048, step = 32)
                     with gr.Row():
-                        scale = gr.Slider(label='Scale', value = 7.5, minimum = 0, maximum=15, step = 0.1)
-                        denoise_strength = gr.Slider(label='Denoise Strength', value = 1, minimum = 0, maximum=1, step = 0.1)
+                        scale = gr.Slider(label='Скейл', value = 7.5, minimum = 0, maximum=15, step = 0.1)
+                        denoise_strength = gr.Slider(label='Сила Denoise', value = 1, minimum = 0, maximum=1, step = 0.1)
                     with gr.Row():
-                        seed_input = gr.Textbox(label='Enter Seed here')
-                        scheduler_input = gr.Dropdown(['option 1', 'option 2'])
+                        seed_input = gr.Textbox(label='Ключ генерации')
+                        scheduler_input = gr.Dropdown(['Опция 1', 'Опция 2'])
                         
-                    num_image = gr.Slider(label='Num. of Images', value = 1, minimum = 1, maximum=10, step = 1)
+                    num_image = gr.Slider(label='Количество изображений', value = 1, minimum = 1, maximum=10, step = 1)
                         
-                    #prompt_input_negative = gr.Textbox()
                     with gr.Row():
-                        #load_onnx_model_button = gr.Button('Load Model')
-                        txt2img_button = gr.Button('Generate')
+                        txt2img_button = gr.Button('Сгенерировать')
                     
-                txt2img_output = gr.Image(label='Output Image')
+                txt2img_output = gr.Image(label='Итоговое изображение')
         with gr.Tab('img2img'):
-            img2img_prompt_input = gr.Textbox(label='Prompt')
-            img2img_negative_prompt_input = gr.Textbox(label='Negative Prompt')
+            img2img_prompt_input = gr.Textbox(label='')
+            img2img_negative_prompt_input = gr.Textbox(label='Отрицательный запрос')
             with gr.Row():
                 img2img_image_input = gr.Image()
                 
-                img2img_image_output = gr.Image(label='img2img output')
+                img2img_image_output = gr.Image(label='Вывод img2img')
             with gr.Row():
-                    img2img_model_input = gr.Dropdown(label='Select a model', choices = display_onnx_models())
-                    img2img_test_output = gr.Textbox(label='testing output')
-                    img2img_inference_step_input = gr.Slider(label='Steps', value = 30, minimum = 0, maximum=200, step = 1)
+                    img2img_model_input = gr.Dropdown(label='Выберите модель:', choices = display_onnx_models())
+                    img2img_test_output = gr.Textbox(label='Статус модели')
+                    img2img_inference_step_input = gr.Slider(label='Steps', value = 30, minimum = 5, maximum=100, step = 1)
             with gr.Row():
-                img2img_image_height = gr.Slider(label='Height', value = 512, minimum = 0, maximum=1080, step = 64)
-                img2img_image_width = gr.Slider(label='Width', value = 512, minimum = 0, maximum=1080, step = 64)
+                img2img_image_height = gr.Slider(label='Высота', value = 512, minimum = 0, maximum=2048, step = 64)
+                img2img_image_width = gr.Slider(label='Ширина', value = 512, minimum = 0, maximum=2048, step = 64)
             with gr.Row():
-                img2img_scale = gr.Slider(label='Scale', value = 7.5, minimum = 0, maximum=15, step = 0.1)
-                img2img_denoise_strength = gr.Slider(label='Denoise Strength', value = 1, minimum = 0, maximum=1, step = 0.1)
+                img2img_scale = gr.Slider(label='Скейл', value = 7.5, minimum = 0, maximum=15, step = 0.1)
+                img2img_denoise_strength = gr.Slider(label='Сила Denoise', value = 1, minimum = 0, maximum=1, step = 0.1)
             with gr.Row():
-                with gr.Row():
-                    img2img_seed_input = gr.Textbox(label='Enter Seed here')
-                    img2img_num_image = gr.Slider(label='Num. of Images', value = 1, minimum = 1, maximum=10, step = 1)
-                img2img_scheduler_input = gr.Dropdown(['option 1', 'option 2'])
+                img2img_seed_input = gr.Textbox(label='Ключ генерации')
+                img2img_num_image = gr.Slider(label='Количество изображений', value = 1, minimum = 1, maximum=10, step = 1)
                 
                 
-            img2img_button = gr.Button('Generate')
-        with gr.Tab('Model Manager'):
-            gr.Markdown("Some of the models require you logging in to Huggingface and agree to their terms. Make sure to do that before downloading models!")
+            img2img_button = gr.Button('Сгенерировать')
+        with gr.Tab('Менеджер моделей'):
+            gr.Markdown("Некоторые модели требуют, чтобы вы вошли в Huggingface и согласились с их условиями. Обязательно сделайте это перед загрузкой моделей!")
             model_download_input = gr.Textbox()
-            model_download_button = gr.Button('Download Model')
-            model_dir_refresh = gr.Button('Refresh')
+            model_download_button = gr.Button('Установить модель')
+            model_dir_refresh = gr.Button('Обновить')
             
-        with gr.Tab('Settings'):
-            gr.HTML("Click on this link to find your <a href='https://huggingface.co/settings/tokens' style='color:blue'>Huggingface Access Token</a>")
+        with gr.Tab('Настройки'):
+            gr.HTML("Нажмите на эту ссылку чтобы получить свой API ключ: <a href='https://huggingface.co/settings/tokens' style='color:blue'>HuggingFace Access Token</a>")
             hugginface_token_input = gr.Textbox()
             huggingface_login_message = gr.Textbox()
-            huggingface_login_button = gr.Button('Login HuggingFace')
+            huggingface_login_button = gr.Button('Войти в HuggingFace')
             
         txt2img_button.click(txt2img, inputs=[txt2img_prompt_input, txt2img_negative_prompt_input, inference_step_input,
                                                                                   image_height, image_width, scale, denoise_strength,
                                                                                   seed_input, scheduler_input,
                                                                                   num_image], outputs = txt2img_output)
         
-        #img2img_button.click(img2img, inputs = [img2img_prompt_input, img2img_image_input], outputs= img2img_image_output, show_progress=True)
-        
         img2img_button.click(img2img, inputs=[img2img_prompt_input, img2img_negative_prompt_input, img2img_image_input, img2img_inference_step_input,
                                                                                   img2img_image_height, img2img_image_width, img2img_scale, img2img_denoise_strength,
-                                                                                  img2img_seed_input, img2img_scheduler_input, img2img_num_image
+                                                                                  img2img_seed_input, img2img_num_image
                                                                                   ], outputs = img2img_image_output, show_progress=True)
 
 
@@ -261,8 +223,6 @@ def start_app():
                                        outputs = huggingface_login_message)
         
         model_download_button.click(download_sd_model, inputs = model_download_input)
-        
-        #load_onnx_model_button.click(load_onnx_model, inputs=txt2img_model_input, show_progress=True, outputs = test_output)
         txt2img_model_input.change(load_onnx_model, inputs=txt2img_model_input, show_progress=True, outputs = test_output)
         img2img_model_input.change(load_onnx_model_i2i, inputs=img2img_model_input, show_progress=True, outputs = img2img_test_output)
 
--- start_app.py
+++ start_app.py
@@ -16,8 +16,8 @@ elif sys.version[:3] == "3.8":
 elif sys.version[:3] == "3.7":
     onnx_nightly = 'ort_nightly_directml-1.13.0.dev20220908001-cp37-cp37-win_amd64.whl'
 else:
-    print('no support python version found, install any of the 3.7, 3.8, 3.9, or 3.10!')
-print(f'You are using python version - {sys.version}')
+    print('Найдена версия которая не поддерживает DirectML, пожалуйста установите 3.7, 3.8, 3.9, или 3.10!')
+print(f'Вы используете Python {sys.version}')
 
 
 
@@ -29,13 +29,13 @@ requirements = pathlib.Path().absolute()  /'requirements.txt'
 
 
 def pip_install(lib):
-    subprocess.run(f'echo Installing {lib}...', shell=True)
+    subprocess.run(f'echo Устанавливаю {lib}...', shell=True)
     subprocess.run(f'echo "{python}" -m pip install {lib}', shell=True)
     subprocess.run(f'"{python}" -m pip install {lib}', shell=True, capture_output=True)
     subprocess.run(f'"{python}" -m pip install {lib}', shell=True, capture_output=True)
 
 def pip_install_requirements():
-    subprocess.run(f'echo installing requirements', shell=True)
+    subprocess.run(f'echo Устанавливаю requirements.txt', shell=True)
     subprocess.run(f'"{python}" -m pip install -r requirements.txt', shell=True, capture_output=True)
 
 def is_installed(lib):
@@ -49,20 +49,11 @@ def git_clone(repo_url, repo_name):
         subprocess.run(f'echo cloning {repo_dir}', shell=True)
         subprocess.run(f'git clone {repo_url} "{repo_dir}"', shell=True)
     else:
-        subprocess.run(f'echo {repo_name} already exists!', shell=True)
+        subprocess.run(f'echo {repo_name} уже существует!', shell=True)
 
-
-#git_clone('https://github.com/huggingface/diffusers','diffusers')
-
-#subprocess.run(rf'echo "{python}" -m pip install -e .\repositories\diffusers', shell=True)
-#subprocess.run(rf'"{python}" -m pip install -e .\repositories\diffusers', shell=True, capture_output=True)
-
-#for lib in required_lib:
- #   if not is_installed(lib):
- #       pip_install(lib)
 pip_install_requirements()
 subprocess.run(f'"{python}" -m pip install repositories/{onnx_nightly}', shell=True)
-subprocess.run('echo Done installing', shell=True)
+subprocess.run('echo Успешная установка', shell=True)
 
 
 
